c46798ef2196ee4447d6086a83e05ae118ab193c
HakimP <pedrohakim0@gmail.com>
2016-08-04 21:49:17 -0500

    guardado en bd ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,9 @@ class PagesController < ApplicationController
 
   def batman_vs_superman
   end
+
+  def save_form
+  	Voto.create(superheroe: params[:q], email: params[:r])
+  	redirect_to pages_batman_vs_superman_path, notice: "tu voto fue guardado"
+  end
 end
